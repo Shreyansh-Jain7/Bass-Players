a75b12195db54b210892fb20e48438fd28adb624
Shreyansh-Jain7 <shreyansh712jain@gmail.com>
2023-02-26 15:41:20 +0530

    chsndn

--- Scripts/category.js
+++ Scripts/category.js
@@ -1,5 +1,23 @@
 
+let av=localStorage.getItem("loggedin");
+console.log(av);
 
+if(av!=null){
+    document.querySelector("#avt").innerHTML=null;
+    console.log(av);
+    let logo=document.createElement("h1");
+    logo.innerText=av;
+    logo.style.fontSize="40px"
+    logo.style.marginTop="15px";
+    logo.style.marginLeft="15px"
+    logo.style.color="white";
+    document.querySelector("#avt").append(logo);
+    document.querySelector("#avt").style.width="50px";
+    document.querySelector("#avt").style.height="50px";
+    document.querySelector("#avt").style.padding="auto";
+    document.querySelector("#avt").style.backgroundColor="black";
+    document.querySelector("#avt").style.borderRadius="50%";
+}
 let fetchedData=[];
 
 document.querySelectorAll('.prices').forEach(x => {
--- Scripts/index.js
+++ Scripts/index.js
@@ -8,3 +8,23 @@ document.querySelector("#sonar").addEventListener("click",()=>{
     localStorage.setItem("cat","sonar");
     window.location.href="./category.html";
 })
+
+let av=localStorage.getItem("loggedin");
+console.log(av);
+
+if(av!=null){
+    document.querySelector("#avt").innerHTML=null;
+    console.log(av);
+    let logo=document.createElement("h1");
+    logo.innerText=av;
+    logo.style.fontSize="40px"
+    logo.style.marginTop="15px";
+    logo.style.marginLeft="15px"
+    logo.style.color="white";
+    document.querySelector("#avt").append(logo);
+    document.querySelector("#avt").style.width="50px";
+    document.querySelector("#avt").style.height="50px";
+    document.querySelector("#avt").style.padding="auto";
+    document.querySelector("#avt").style.backgroundColor="black";
+    document.querySelector("#avt").style.borderRadius="50%";
+}
